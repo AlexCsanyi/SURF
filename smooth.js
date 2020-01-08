34ff1b3cc3661d4f035f96504dfb9fcc2c85d96f
AlexCsanyi <alex.csanyi@gmail.com>
2020-01-08 07:03:15 +0000

    wrote my own smooth scroll function

--- smooth.js
+++ smooth.js
@@ -1,9 +1,32 @@
-import easyScroll from "easy-scroll";
+function smoothScroll(target, duration) {
+  var target = document.querySelector(target);
+  var targetPosition = target.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - startPosition;
+  var startTime = null;
 
-easyScroll({
-  scrollableDomEle: window,
-  direction: "bottom",
-  duration: 2000,
-  easingPreset: "easeInQuad",
-  scrollAmount: 1000
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * Math.pow(2, 10 * (t - 1)) + b;
+    t--;
+    return (c / 2) * (-Math.pow(2, -10 * t) + 2) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+var navElement1 = document.getElementById("nav-guide");
+var navElement2 = document.getElementById("nav-program");
+var navElement3 = document.getElementById("nav-gallery");
+
+navElement1.addEventListener("click", function() {
+  smoothScroll("#guide", 1000);
 });
