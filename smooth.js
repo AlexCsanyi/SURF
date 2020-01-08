2961db1620a4c81c221fea87ca5a397e91040eec
AlexCsanyi <alex.csanyi@gmail.com>
2020-01-08 06:34:34 +0000

    smooth scroll added

---
+++ smooth.js
@@ -0,0 +1,9 @@
+import easyScroll from "easy-scroll";
+
+easyScroll({
+  scrollableDomEle: window,
+  direction: "bottom",
+  duration: 2000,
+  easingPreset: "easeInQuad",
+  scrollAmount: 1000
+});
